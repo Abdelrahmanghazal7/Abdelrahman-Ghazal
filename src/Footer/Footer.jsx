a53144978800e92c56a47a6769ee275b0c2c6d26
Abdelrahmanghazal7 <abdelrahmanghazal7@gmail.com>
2023-03-06 20:42:02 +0200

    ll

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,75 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import "./Footer.css";
-import "./style.css";
 import Aos from "aos";
 import "aos/dist/aos.css";
 
 const Footer = () => {
-
-  document.querySelectorAll('.button').forEach(button => {
-    let duration = 3000,
-      svg = button.querySelector('svg'),
-      svgPath = new Proxy({
-        y: null,
-        smoothing: null
-      }, {
-        set(target, key, value) {
-          target[key] = value;
-          if (target.y !== null && target.smoothing !== null) {
-            svg.innerHTML = getPath(target.y, target.smoothing, null);
-          }
-          return true;
-        },
-        get(target, key) {
-          return target[key];
-        }
-      });
-
-    button.style.setProperty('--duration', duration);
-    svgPath.y = 20;
-    svgPath.smoothing = 0;
-    button.addEventListener('click', e => {
-      e.preventDefault();
-
-      if (!button.classList.contains('loading')) {
-        button.classList.add('loading');
-        setTimeout(() => {
-          svg.innerHTML = getPath(0, 0, [
-            [3, 14],
-            [8, 19],
-            [21, 6]
-          ]);
-        }, duration / 2);
-      }
-    });
-  });
-
-  function getPoint(point, i, a, smoothing) {
-    let cp = (current, previous, next, reverse) => {
-      let p = previous || current,
-        n = next || current,
-        o = {
-          length: Math.sqrt(Math.pow(n[0] - p[0], 2) + Math.pow(n[1] - p[1], 2)),
-          angle: Math.atan2(n[1] - p[1], n[0] - p[0])
-        },
-        angle = o.angle + (reverse ? Math.PI : 0),
-        length = o.length * smoothing;
-      return [current[0] + Math.cos(angle) * length, current[1] + Math.sin(angle) * length];
-    },
-      cps = cp(a[i - 1], a[i - 2], point, false),
-      cpe = cp(point, a[i - 1], a[i + 1], true);
-    return `C ${cps[0]},${cps[1]} ${cpe[0]},${cpe[1]} ${point[0]},${point[1]}`;
-  }
-
-  function getPath(update, smoothing, pointsNew) {
-    let points = pointsNew ? pointsNew : [
-      [4, 12],
-      [12, update],
-      [20, 12]
-    ],
-      d = points.reduce((acc, point, i, a) => i === 0 ? `M ${point[0]},${point[1]}` : `${acc} ${getPoint(point, i, a, smoothing)}`, '');
-    return `<path d="${d}" />`;
-  }
 
   // Download button is hidden at the beginning
   const [showButton, setShowButton] = useState(false);
@@ -147,17 +81,8 @@ const Footer = () => {
               <span>
 
                 {showButton && (
-                  <a href="" download title='Download CV' className="download-btn button dark-single">
-                    <div>
-                      <svg viewBox="0 0 24 24"></svg>
-                    </div>
-                  </a>
+                  <a href="CV.pdf" download title='Download CV' className="download-btn"><i className="fa-solid fa-download"></i></a>
                 )}
-
-                {/* dribbble  */}
-                <a className="dribbble" href="https://dribbble.com/shots/7299868-Download-Buttons" target="_blank">
-                  <img src="https://cdn.dribbble.com/assets/dribbble-ball-mark-2bd45f09c2fb58dbbfb44766d5d1d07c5a12972d602ef8b32204d28fa3dda554.svg" alt="" />
-                  </a>
 
               </span>
             </div>
