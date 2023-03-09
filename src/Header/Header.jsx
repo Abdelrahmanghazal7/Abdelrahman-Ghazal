fb24d1082637e9fe666f079d08119c2997101892
Abdelrahmanghazal7 <abdelrahmanghazal7@gmail.com>
2023-03-10 00:13:37 +0200

    ll

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -3,6 +3,11 @@ import Typewriter from 'typewriter-effect';
 import './Header.css'
 import Aos from "aos";
 import "aos/dist/aos.css";
+import { GrFacebookOption } from "react-icons/gr";
+import { AiFillInstagram } from "react-icons/ai";
+import { RiWhatsappFill } from "react-icons/ri";
+import { AiFillGithub } from "react-icons/ai";
+import { AiFillLinkedin } from "react-icons/ai";
 
 const Header = () => {
   const [state] = useState({
@@ -39,11 +44,11 @@ const Header = () => {
             <p data-aos="fade-down-right">developer, designer, engineer</p>
           </div>
           <ul data-aos="slide-left" className="list-unstyled list-social">
-            <li><a target="_blank" href="https://www.facebook.com/abdelrahmanghazal77y?mibextid=ZbWKwL"><i className="icon-3d fa-brands fa-facebook-f"></i></a></li>
-            <li><a target="_blank" href="https://instagram.com/abdelrahmanghazal_7?igshid=NTE5MzUyOTU="><i id='fa-4x' className="fa fa-instagram fa-4x icon-3d"></i></a></li>
-            <li><a target="_blank" href="https://wa.me/qr/32ZJYFUAYSO4G1"><i className="icon-3d fa-brands fa-whatsapp"></i></a></li>
-            <li><a target="_blank" href="https://github.com/Abdelrahmanghazal7"><i className="icon-3d fa-brands fa-github"></i></a></li>
-            <li><a target="_blank" href="https://www.linkedin.com/in/abdelrahman-ghazal-b046781ba?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Bu4h2%2BCJcRM6195%2FqfBRa4Q%3D%3D"><i className="fa-brands fa-linkedin icon-3d"></i></a></li>
+            <li><a className='icxn' target="_blank" href="https://www.facebook.com/abdelrahmanghazal77y?mibextid=ZbWKwL"><GrFacebookOption/></a></li>
+            <li><a className='icxn' target="_blank" href="https://instagram.com/abdelrahmanghazal_7?igshid=NTE5MzUyOTU="><AiFillInstagram/></a></li>
+            <li><a className='icxn' target="_blank" href="https://wa.me/qr/32ZJYFUAYSO4G1"><RiWhatsappFill/></a></li>
+            <li><a className='icxn' target="_blank" href="https://github.com/Abdelrahmanghazal7"><AiFillGithub/></a></li>
+            <li><a className='icxn' target="_blank" href="https://www.linkedin.com/in/abdelrahman-ghazal-b046781ba?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Bu4h2%2BCJcRM6195%2FqfBRa4Q%3D%3D"><AiFillLinkedin/></a></li>
           </ul>
         </div>
       </div>
